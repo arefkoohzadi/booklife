02f36667fe322fc20e590e1b20ae3bbaae655ddc
aref koohzadi <aref.koohzadi@gmail.com>
2020-08-15 13:36:16 +0430

    add font BYekan to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = (props) => {
-  return <div className="">BookLife!</div>;
+  return (
+    <div className="flex flex-row">
+      <h1 className="font-BYekan text-5xl mr-64">زندگی کتاب</h1>
+    </div>
+  );
 };
 
 export default App;
