2839d3e960c4ebcf22ad4c3a723988f7c967bd1e
aref koohzadi <aref.koohzadi@gmail.com>
2020-09-24 21:22:31 +0330

    Updates

---
+++ precache-manifest.dec164f88ee9b4dbefff8c373f0329d5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8048dd6695111924e74e95caf1769a80",
+    "url": "/booklife/index.html"
+  },
+  {
+    "revision": "5f8e8149d14bd482edf2",
+    "url": "/booklife/static/css/2.f4bde6ca.chunk.css"
+  },
+  {
+    "revision": "fa0eb85386c966a94fe2",
+    "url": "/booklife/static/css/main.25e0990a.chunk.css"
+  },
+  {
+    "revision": "5f8e8149d14bd482edf2",
+    "url": "/booklife/static/js/2.ed40258b.chunk.js"
+  },
+  {
+    "revision": "b59c534e57866f823b154707be4a3da1",
+    "url": "/booklife/static/js/2.ed40258b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917caa7df3eab3fe380b",
+    "url": "/booklife/static/js/3.3d7a270b.chunk.js"
+  },
+  {
+    "revision": "69b1a985d21d111ebf52",
+    "url": "/booklife/static/js/4.d4981eb6.chunk.js"
+  },
+  {
+    "revision": "fa0eb85386c966a94fe2",
+    "url": "/booklife/static/js/main.e742cced.chunk.js"
+  },
+  {
+    "revision": "6dcb5ba9269183a845f4",
+    "url": "/booklife/static/js/runtime-main.f06fa8a3.js"
+  },
+  {
+    "revision": "0b5055ac357359f8c23320ea3dc0f78b",
+    "url": "/booklife/static/media/IRANSansWeb.0b5055ac.woff2"
+  },
+  {
+    "revision": "38d4b5fba7a4955e035b5a3898a8cd5d",
+    "url": "/booklife/static/media/IRANSansWebFaNum.38d4b5fb.woff2"
+  },
+  {
+    "revision": "84518599d382334f09e67ade7fc530a0",
+    "url": "/booklife/static/media/IRANSansWebFaNumMedium.84518599.woff2"
+  },
+  {
+    "revision": "43a0ecf3c7f2af819b192d1284f95ed9",
+    "url": "/booklife/static/media/IRANSansWeb_Bold.43a0ecf3.woff2"
+  },
+  {
+    "revision": "9c66b762719d40d1f18e678a1405459a",
+    "url": "/booklife/static/media/IRANSansWeb_Medium.9c66b762.woff2"
+  },
+  {
+    "revision": "5f0682a6ebb6b30458d02d5fb777d31c",
+    "url": "/booklife/static/media/cover.5f0682a6.gif"
+  },
+  {
+    "revision": "a002300217b95fbe56245dae62c395d7",
+    "url": "/booklife/static/media/photo.a0023002.jpg"
+  }
+]);
